0bd767400d8f6b9674811de0ea0e5282bedfb34d
BlackyFox <puissant@et.esiea.fr>
2014-12-08 11:46:11 +0100

    progress bar

--- AppDevMobil/app/src/main/java/com/example/blackyfox/appdevmobil/MainActivity2.java
+++ AppDevMobil/app/src/main/java/com/example/blackyfox/appdevmobil/MainActivity2.java
@@ -1,15 +1,31 @@
 package com.example.blackyfox.appdevmobil;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.io.OutputStream;
+
 
 public class MainActivity2 extends Activity {
+    private ProgressBar MyPB;
+    private TextView tpro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +35,10 @@ public class MainActivity2 extends Activity {
         String ms = getIntent().getStringExtra(MainActivity.VALUE);
 
         TextView tv1 = (TextView) findViewById(R.id.g_value);
+        MyPB = (ProgressBar)findViewById(R.id.pBAsync);
+        tpro = (TextView)findViewById(R.id.tPro);
         tv1.setText(ms);
+        new MyAsyncTsk().execute(ms);
     }
 
 
@@ -43,5 +62,53 @@ public class MainActivity2 extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class MyAsyncTsk extends AsyncTask<String, Integer, Void>{
+        String s = null;
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            Toast.makeText(getApplicationContext(), "Start", Toast.LENGTH_SHORT).show();
+        }
+        @Override
+        protected void onProgressUpdate(Integer... values){
+            super.onProgressUpdate(values);
+            MyPB.setProgress(values[0]);
+            tpro.setText(values[0].toString()+"%");
+
+        }
+        @Override
+        protected Void doInBackground(String... strings){
+            Looper.prepare();
+            s = strings[0];
+            int progress;
+            for (progress=0;progress<=100;progress++)
+            {
+                for (int i=0; i<10000000; i++){}
+                publishProgress(progress);
+                progress++;
+            }
+
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result){
+            Log.d("STRING", "s = "+s);
+            Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+            LinearLayout ll = (LinearLayout) findViewById(R.id.root);
+            ll.setBackgroundColor(Color.argb(255, 0, 255, 0));
+        }
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            //setContentView(R.layout.activity_main_activity2);
+
+        } else {
+            //setContentView(R.layout.activity_main_activity2);
+        }
     }
 }
