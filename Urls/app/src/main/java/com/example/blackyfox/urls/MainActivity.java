f3c884111abcc9705e7b2e46644361c4d2edb963
BlackyFox <puissant@et.esiea.fr>
2014-12-16 15:48:14 +0100

    Ajout IntentService

--- Urls/app/src/main/java/com/example/blackyfox/urls/MainActivity.java
+++ Urls/app/src/main/java/com/example/blackyfox/urls/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.blackyfox.urls;
 
 import android.app.Activity;
+import android.app.IntentService;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
@@ -23,28 +28,45 @@ import java.net.URL;
 public class MainActivity extends Activity {
     Button jsonbut;
     TextView jsonText;
+    public static final String BROADCAST_ACTION = "com.example.blackyfox.urls.TRANSACTION_DONE";
+    private MyReceiver receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        IntentFilter filter = new IntentFilter(BROADCAST_ACTION);
+        filter.addCategory(Intent.CATEGORY_DEFAULT);
+        receiver = new MyReceiver();
+        registerReceiver(receiver, filter);
+
         jsonbut = (Button) findViewById(R.id.jbut);
         jsonText = (TextView) findViewById(R.id.jsonres);
 
         jsonbut.setOnClickListener(jclick);
     }
 
+    private class MyReceiver extends BroadcastReceiver{
+        public void onReceive(Context context, Intent intent){
+            String content = intent.getStringExtra("content");
+            jsonText.setText(content);
+        }
+    }
+
     private View.OnClickListener jclick = new OnClickListener(){
         public void onClick(View v){
-            new DLTask().execute("http://fabrigli.fr/cours/example.json");
+            Intent i = new Intent(MainActivity.this, DLIntentService.class);
+            i.putExtra("request", "http://fabrigli.fr/cours/example.json");
+            startService(i);
         }
     };
 
-    private class DLTask extends AsyncTask<String, Void, String> {
+    /*private class DLTask extends AsyncTask<String, Void, String> {
         @Override
         protected String doInBackground(String... urls){
             try{
-                return downloadUrl(urls[0]);
+                //TODO
             }catch(IOException e){
                 return"Unable to retrieve the Web Page.";
             }
@@ -55,34 +77,7 @@ public class MainActivity extends Activity {
             //Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
         }
     }
-
-    private String downloadUrl(String myUrl) throws IOException{
-        InputStream is = null;
-        try{
-            URL url = new URL(myUrl);
-            HttpURLConnection connec = (HttpURLConnection) url.openConnection();
-            connec.setRequestMethod("GET");
-            connec.connect();
-            int reponse = connec.getResponseCode();
-            is = connec.getInputStream();
-
-            String contentAsString = readIt(is, 500);
-            return(contentAsString);
-        }finally {
-            if(is != null){
-                is.close();
-            }
-        }
-    }
-
-    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
-        Reader r = null;
-        r = new InputStreamReader(stream, "UTF-8");
-        char[] buf = new char[len];
-        r.read(buf);
-        return new String(buf);
-    }
-
+*/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
